5e9d806828fc754779e8fa85138a2e601bc63cbe
Saijaswanthi <jassu.hup@gmail.com>
2024-10-29 18:41:51 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'user_auth'
+});
+
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('MySQL connected...');
+});
+
+module.exports = db;
